7269085b353c534769131447cc9939760b36357c
zaskiajd <zaskiajanualitadevi@domain.com>
2026-02-25 19:12:29 +0700

    penambahan connection

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'kampus';
+$username = 'user';
+$password = 'password';
+try {
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username,
+$password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+die("Koneksi Database Gagal: " . $e->getMessage());
+}
+?>
